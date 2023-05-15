2c9124e2df87b58ba8494305f2906c3519de7ac4
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-15 15:12:55 +1000

    class file

---
+++ linked list.java
@@ -0,0 +1,13 @@
+public class linked list{
+
+    public static void main(String[] args){
+        linked list<String>al= new linked list<String>();
+        al.add("Farhan");
+        al.add("Ravi");
+        al.add("vijay");
+        al.add("Ram");
+        iterator<String> itr = al.iterator();
+        While(it.hasNext()){         
+        }
+    }
+}
