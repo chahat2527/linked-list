4a0e510490a94e74aff71ad4cad197e8d7d1d257
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-15 15:29:19 +1000

    class file

---
+++ Reverselink.java
@@ -0,0 +1,13 @@
+public class reverse {
+    public static void main(String[] args){
+        linked list<String>||= new linked list <String>();
+        ||.add("Ravi");
+        ||.add("Ram");
+        ||.add("Vijay");
+        //Traversing the list of elements in reverse order
+        iterator=||. descending iterator();
+        while(i.hasNext());{
+            System.out.println(i.next());
+        }
+    }
+}
